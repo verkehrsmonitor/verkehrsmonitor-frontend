4b214c5acd42b70b20a90d046e40139ab77fe64e
moklick <info@moritzklack.com>
2018-12-18 12:29:43 +0100

    refactor(about): update text

--- src/pages/About.js
+++ src/pages/About.js
@@ -24,10 +24,11 @@ const About = () => (
       <h2>Über das Projekt</h2>
       <p>
         Der verkehrsmonitor gibt eine Übersicht über die automatischen Zählstellen an Autobahnen und Bundesstrßaen in Deutschland zwischen 2003 und 2016.
-        Es ist möglich einen Zeitraum, ein Bundesland oder eine Straße zu filtern. Die Daten für dieses Projekt wurden vom <a target="_blank" href="https://www.bast.de/BASt_2017/DE/Home/home_node.html">BASt</a> erfasst und auf deren <a target="_blank" href="https://www.bast.de/BASt_2017/DE/Verkehrstechnik/Fachthemen/v2-verkehrszaehlung/Verkehrszaehlung.html">Webseite</a> zur Verfügung gestellt.
+        Es ist möglich einen Zeitraum, ein Bundesland oder eine Straße zu filtern.
+        Die Verkehrsmengendaten, auf die in diesem Projekt zugegriffen wird, wurden im Auftrag der <a target="_blank" href="https://www.bast.de/BASt_2017/DE/Home/home_node.html">BASt</a> von den Bundesländern erfasst, von der BASt aufbereitet und auf ihrer <a target="_blank" href="https://www.bast.de/BASt_2017/DE/Verkehrstechnik/Fachthemen/v2-verkehrszaehlung/Verkehrszaehlung.html">Webseite zur Verfügung gestellt</a>.
       </p>
       <p>
-        Der verkehrsmonitor ist im Rahmen einer Vorstudie einer <a href="https://www.bmvi.de/DE/Themen/Digitales/mFund/Ueberblick/ueberblick.html" target="_blank">mFUND</a> Förderung entstanden und wurde von der <a href="https://webkid.io" target="_blank">webkid GmbH</a>.
+        Der verkehrsmonitor ist im Rahmen einer Vorstudie einer <a href="https://www.bmvi.de/DE/Themen/Digitales/mFund/Ueberblick/ueberblick.html" target="_blank">mFUND</a> Förderung entstanden und wurde von der <a href="https://webkid.io" target="_blank">webkid GmbH</a> konzeptioniert und entwickelt.
       </p>
     </StyledPageWrapper>
   </StylesPage>
